ec1842adb934b1cef48d42219f4bfbab3e2e1def
HoangThang21 <nt0912794191@gmail.com>
2024-03-22 09:35:10 +0700

    ...

--- example-app/database/migrations/2024_02_26_073143_create_user.php
+++ example-app/database/migrations/2024_02_26_073143_create_user.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('sdt')->nullable();
             $table->string('email');
             $table->string('password');
             $table->string('image')->nullable();
--- example-app/resources/views/layouts/top.blade.php
+++ example-app/resources/views/layouts/top.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Music - Nghe nhạc mới HOT nhất, tải nhạc MP3 chất lượng cao</title>
+    <title>Music - Nghe nhạc chờ mới HOT nhất, tải nhạc MP3 chất lượng cao</title>
     <link rel="shortcut icon" href="../../images/logomobifone.png" type="image/png">
     <link href="../../inc/css/index.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
